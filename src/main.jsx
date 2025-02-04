b7688693af54e95d36d07eb358356e165f4ec150
JesusBTE <jeisbetancourtes@ittepic.edu.mx>
2025-02-03 17:40:14 -0700

    Puntos realizados D-F

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './App.jsx'
-//import MyFunctions from "./fuctions/MyFunctions.jsx";
-//import MyPromises from './functions/MyPromises.jsx';
-import MyPromisesAll from './functions/MyPromisesAll';
 import './index.css'
+//import App from './App.jsx'
+//import MyFunctions from './functions/MyFunctions.jsx'
+//import MyPromises from './functions/MyPromises.jsx'
+//import MyPromisesAll from './functions/MyPromisesAll.jsx'
+//import MyPromisesRace from './functions/MyPromisesRace.jsx'
+import MyFetchAPIs from './functions/MyFetchAPIs.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <MyFunctions/> */}
-    {/*<MyPromises/> */}
-    <MyPromisesAll/>
+      {/*<App />*/}  
+      {/*<MyFunctions />*/}
+      {/*<MyPromises/>*/}
+      {/*<MyPromisesAll/>*/}
+      {/*<MyPromisesRace/>*/}
+      <MyFetchAPIs/>
   </StrictMode>,
-);
-
+)
